71ad080b2b58f2a4447e84cb8e610320e38bc9a1
Franco Marolla <franco@MacBook-Pro.local>
2024-12-10 14:41:58 -0300

    Agrego variables de entorno

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDUjSCju003CsGLgT2SxteqL5JK-fr40eE",
-  authDomain: "soldierhopsdb-b04b0.firebaseapp.com",
-  projectId: "soldierhopsdb-b04b0",
-  storageBucket: "soldierhopsdb-b04b0.firebasestorage.app",
-  messagingSenderId: "511025970896",
-  appId: "1:511025970896:web:74b75cfdf4e5dd75b168d5"
+  apiKey: import.meta.env.VITE_API,
+  authDomain: import.meta.env.VITE_AUTH,
+  projectId: import.meta.env.VITE_PROJECT, 
+  storageBucket: import.meta.env.VITE_STORAGE,
+  messagingSenderId: import.meta.env.VITE_MESSAGING,
+  appId: import.meta.env.VITE_APPID, 
 };
 
 
