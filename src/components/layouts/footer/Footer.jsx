461b1d958db269904ce51261f7f2d701861ee733
Franco Marolla <franco@MacBook-Pro.local>
2024-12-17 15:33:26 -0300

    Agrego copyright al footer

--- src/components/layouts/footer/Footer.jsx
+++ src/components/layouts/footer/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Footer.css'
+import CopyrightIcon from '@mui/icons-material/Copyright';
 
 const Footer = () => {
   return (
     <div className='footer-container'>
-        <h2 className='footer-title'>Soldier Hops</h2>
+        <h2 className='footer-title'>Soldier Hops <CopyrightIcon fontSize='small'></CopyrightIcon></h2>
 
     </div>
   )
